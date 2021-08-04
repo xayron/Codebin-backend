f61f1951f25c5fe8b1cae37e71bef327e3124fed
Debjit Chakraborty <debjit.c@highradius.com>
2021-08-04 21:31:29 +0530

    Test url mapping

--- src/main/java/com/codebin/backend/CodebinBackendApplication.java
+++ src/main/java/com/codebin/backend/CodebinBackendApplication.java
@@ -2,6 +2,7 @@ package com.codebin.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CodebinBackendApplication {
@@ -9,5 +10,11 @@ public class CodebinBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CodebinBackendApplication.class, args);
 	}
+}
 
+class HelloController {
+	@GetMapping("/")
+	String hello() {
+		return "Hello World";
+	}
 }
