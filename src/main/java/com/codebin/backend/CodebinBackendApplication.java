aff835d0fb25985e9e7fa3f525bdb46687c2d3a4
Debjit Chakraborty <debjit.c@highradius.com>
2021-08-04 21:42:40 +0530

    Fix errors

--- src/main/java/com/codebin/backend/CodebinBackendApplication.java
+++ src/main/java/com/codebin/backend/CodebinBackendApplication.java
@@ -3,6 +3,7 @@ package com.codebin.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CodebinBackendApplication {
@@ -16,6 +17,6 @@ public class CodebinBackendApplication {
 class HelloController {
 	@GetMapping("/")
 	String hello() {
-		return "Hello World";
+		return "Hello World. Welcome to my app";
 	}
 }
